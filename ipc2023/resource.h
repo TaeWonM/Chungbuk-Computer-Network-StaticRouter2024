4e343d081672338c3a1434fb46d9078c96770902
TaeWonM <minququ123456@gmail.com>
2024-11-21 16:07:46 +0900

    Add and Revise User Interface

--- ipc2023/resource.h
+++ ipc2023/resource.h
@@ -2,12 +2,14 @@
 // Microsoft Visual C++에서 생성한 포함 파일입니다.
 // ipc2023.rc에서 사용되고 있습니다.
 //
+#define IDOK2                           3
 #define IDM_ABOUTBOX                    0x0010
 #define IDD_ABOUTBOX                    100
 #define IDS_ABOUTBOX                    101
 #define IDD_IPC2023_DIALOG              102
 #define IDR_MAINFRAME                   128
 #define IDD_DIALOG1                     130
+#define IDD_DIALOG2                     132
 #define IDC_LIST1                       1000
 #define IDC_LIST_CHAT                   1000
 #define IDC_SRC_IP                      1000
@@ -17,9 +19,11 @@
 #define IDC_EDIT2                       1002
 #define IDC_EDIT_DST                    1002
 #define IDC_PROXY_EDIT                  1002
+#define IDC_EDIT_SRC3                   1002
 #define IDC_EDIT3                       1003
 #define IDC_EDIT_MSG                    1003
 #define IDC_EDIT_GARP                   1003
+#define IDC_SRC_IP2                     1003
 #define bt_send                         1004
 #define IDC_BUTTON_SEND                 1004
 #define bt_setting                      1005
@@ -29,9 +33,15 @@
 #define IDC_CHECK_TOALL                 1006
 #define IDC_EDIT_SRC2                   1006
 #define IDC_GARP_BUTTON_SEND            1006
+#define IDC_BUTTON_UNADDR               1006
+#define IDC_IP_ROUTER_CHECK_UP          1006
 #define IDC_COMBO1                      1007
 #define IDC_EDIT_DST2                   1008
+#define IDC_COMBO2                      1008
+#define IDC_IP_ROUTER_CHECK_GATEWAY     1008
 #define IDC_BUTTON_FOPEN                1009
+#define IDC_CHECK3                      1009
+#define IDC_IP_ROUTER_CHECK_HOST        1009
 #define IDC_BUTTON_FSEND                1010
 #define IDC_PROGRESS_FTRANSFER          1011
 #define IDC_EDIT4                       1012
@@ -45,19 +55,25 @@
 #define IDC_PROXY_COMBO                 1017
 #define IDC_ALL_DELETE_BTN              1018
 #define IDC_PROXY_DST_IP                1018
+#define IDC_LIST4                       1018
 #define IDC_ALL_DELETE_BTN2             1019
+#define IDC_IP_ROUTER_IPADDRESS         1019
 #define IDC_ITEM_DELETE_BTN2            1020
+#define IDC_IP_ROUTER_NET_MASK          1020
 #define IDC_PROXY_DELETE_BTN            1021
 #define IDC_ALL_DELETE_BTN3             1021
+#define IDC_IP_ROUTER_GATEWAY           1021
 #define IDC_PROXY_ITEM_ADD_BTN          1023
+#define IDC_IP_ROUTER_COMBO_INTERFACE   1023
+#define IDC_IP_ROUTING_TABLE_ITEM_ADD_BTN 1024
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_RESOURCE_VALUE        134
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1019
+#define _APS_NEXT_CONTROL_VALUE         1024
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
